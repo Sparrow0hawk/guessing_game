faef9f38fad49e2248c7281c0e330376b459faf9
sparrow0hawk <alexjcoleman@gmail.com>
2021-10-12 17:03:38 +0100

    accepting user input
    includes lockfile

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    let repeated: String = "=".repeat(7);
+    println!("{}", repeated);
+    println!("Guess the number!");
+    println!("{}", repeated);
+
+    println!("Please input your guess:");
+
+    let mut guess = String::new();
+
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("Failed to read line");
+
+    println!("{}", repeated);
+    println!("You guessed: {}", guess);
 }
