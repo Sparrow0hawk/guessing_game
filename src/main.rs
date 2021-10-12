c5ce1cf09bbca826e0edadaaf4564ebfe939db6a
sparrow0hawk <alexjcoleman@gmail.com>
2021-10-12 17:08:55 +0100

    using rand package to generate random number

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,13 @@
 use std::io;
+use rand::Rng;
 
 fn main() {
     let repeated: String = "=".repeat(7);
+
+    let secret = rand::thread_rng().gen_range(1..101);
+
+    println!("The secret number is {}", secret);
+
     println!("{}", repeated);
     println!("Guess the number!");
     println!("{}", repeated);
