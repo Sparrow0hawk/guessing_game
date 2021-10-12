6130161d5bb6990d22c1c41a081d1686b1e68166
sparrow0hawk <alexjcoleman@gmail.com>
2021-10-12 19:46:23 +0100

    convert guess from string to i32 and adding comparison

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use std::io;
 use rand::Rng;
+use std::cmp::Ordering;
 
 fn main() {
     let repeated: String = "=".repeat(7);
@@ -20,6 +21,17 @@ fn main() {
         .read_line(&mut guess)
         .expect("Failed to read line");
 
+    let guess: i32 = guess
+                        .trim()
+                        .parse()
+                        .expect("Please type a number!");
+
     println!("{}", repeated);
     println!("You guessed: {}", guess);
+
+    match guess.cmp(&secret) {
+        Ordering::Less => println!("Too small!"),
+        Ordering::Greater => println!("Too big!"),
+        Ordering::Equal => println!("You win!")
+    }
 }
